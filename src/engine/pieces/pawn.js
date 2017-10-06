c7db164363dbd1f8f98093f80d8fecfe92d88e14
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:40:54 +0100

    test 2 green

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,5 +1,6 @@
 import Piece from './piece';
 import Player from '../player';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -8,8 +9,19 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let currentSquare = board.findPiece(this);
+        let availableMoves =[];
+        if (currentSquare.row === 1) {
+            availableMoves.push(new Square(currentSquare.row+2, currentSquare.col));
+            availableMoves.push(new Square(currentSquare.row+1, currentSquare.col));
+        } else if (currentSquare.row === 6) {
+            availableMoves.push(new Square(currentSquare.row-2, currentSquare.col));
+            availableMoves.push(new Square(currentSquare.row-1, currentSquare.col));
+        } else if (this.player === Player.WHITE) {
+            availableMoves.push(new Square(currentSquare.row+1, currentSquare.col));
+        } else (availableMoves.push(new Square(currentSquare.row-1, currentSquare.col)));
 
-        (this.player === Player.WHITE) ? currentSquare.row++ : currentSquare.row--; 
-        return currentSquare;
+
+        //(this.player === Player.WHITE) ? currentSquare.row++ : currentSquare.row--; 
+        return availableMoves;
     }
 }
