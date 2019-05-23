8c1f3049a3c9dd2bc673021791fe8865a054fe24
Millie <millie_clare@outlook.com>
2019-05-23 15:09:30 +0100

    king tests written and tested. knight tested and refactored

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,18 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        let availableMoves = [];
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (0 === i && 0 === j) {
+                    continue;
+                } else {
+                availableMoves.push(new Square(currentSquare.row + i, currentSquare.col +j));
+                }
+            }
+        }
+        console.log(availableMoves);
+        return availableMoves
     }
 }
--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,20 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        let availableMoves = [];
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                let rowDifference = Math.abs(i - currentSquare.row);
+                let colDifference = Math.abs(j - currentSquare.col)
+                if (currentSquare.row === i && currentSquare.col === j) {
+                    continue;
+                } else if (rowDifference ===  1 && colDifference === 2 || rowDifference ===  2 && colDifference === 1) {
+                    availableMoves.push(new Square(i, j));
+                    console.log(availableMoves);
+                }
+            }
+        }
+        return availableMoves;
     }
 }
